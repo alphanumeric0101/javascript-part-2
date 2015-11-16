03e8bce47a52c8291c9a88dd02ce1226713eb339
alphanumeric0101 <alphanumeric@openmailbox.org>
2015-11-16 19:19:56 +0000

    sort function for strings by length exercise

--- functions.js
+++ functions.js
@@ -1,1 +1,19 @@
 //this is a javascript file
+
+
+//function to sort strings by length, longest first
+
+var animals = ['cat', 'rhino', 'koala', 'cheetah'];
+
+function compare2StringsByLength(a, b) {
+  if (a.length > b.length) {
+    return -1;
+  }
+  if (a.length < b.length) {
+    return 1;
+  }
+  return 0;
+}
+
+console.log(animals.sort(compare2StringsByLength));
+
