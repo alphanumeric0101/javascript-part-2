31ca861b31bace0dcad77e108de892e6311678f8
alphanumeric0101 <alphanumeric@openmailbox.org>
2015-11-16 20:00:45 +0000

    array of objects sorted by length of name and again by alphabetical email

--- functions.js
+++ functions.js
@@ -6,14 +6,50 @@
 var animals = ['cat', 'rhino', 'koala', 'cheetah'];
 
 function compare2StringsByLength(a, b) {
-  if (a.length > b.length) {
+  if (a.name.length > b.name.length) {
     return -1;
   }
-  if (a.length < b.length) {
+  if (a.name.length < b.name.length) {
     return 1;
   }
   return 0;
 }
 
-console.log(animals.sort(compare2StringsByLength));
+function alphabetizeEmails(a,b) {
+  if (a.email < b.email) {
+    return -1;
+  }
+  if (a.email > b.email) {
+    return 1;
+  }
+  return 0;
+}
 
+// console.log(animals.sort(compare2StringsByLength));
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++ //
+
+// array of objects with email and name 
+
+var entries = [
+    {
+        name: "Malcom X",
+        email: "bpp@riseup.net"
+    },
+    {
+        name: "Marc Garvy",
+        email: "nomoss@mail.net"
+    },
+    {
+        name: "Jerry Seinfeld",
+        email: "jcomedy@mail.net"
+    }
+];
+    
+    /*
+        The following logs sort by 'longest name' and 'alphabetic email addresses' respectively
+    
+            console.log(entries.sort(compare2StringsByLength));
+
+            console.log(entries.sort(alphabetizeEmails));
+    */
